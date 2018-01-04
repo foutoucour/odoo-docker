f0e9e9cd15f4a0e7a6b1e0e7d3abfc884c2ac06b
Jordi Riera <kender.jr@gmail.com>
2018-01-03 20:33:57 -0500

    fix wait_pg

--- docker_files/run_test.sh
+++ docker_files/run_test.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-until pg_isready; do
-    echo "$(date) - waiting for postgres..."
-    sleep 1
-done
 odoo -c /etc/odoo/odoo.conf --db_host=db -r odoo -w odoo -d odoo -i main --stop-after-init --test-enable
