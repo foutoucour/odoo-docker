e02184f885784eaa692e4cec2721236747518609
Jordi Riera <kender.jr@gmail.com>
2018-01-03 19:55:49 -0500

    add addons

---
+++ addons/main/tests/test_res_partner_create.py
@@ -0,0 +1,24 @@
+import logging
+
+from odoo.tests import common
+logger = logging.getLogger(__name__)
+
+
+class TestResPartner(common.TransactionCase):
+    """Use case of creation of a partner."""
+    def setUp(self):
+        super(TestResPartner, self).setUp()
+        self.partner_pool = self.env['res.partner']
+        self.country_pool = self.env['res.country']
+        self.canada = self.country_pool.browse(39)
+
+    def test_create(self):
+        partner = self.partner_pool.create({
+            'name': 'tpartner',
+            'state_id': 1,
+            'country_id': self.canada.id,
+        })
+        self.assertEqual(1, partner.id)
+
+    def test_fake(self):
+        self.assertEqual(1, 2)
