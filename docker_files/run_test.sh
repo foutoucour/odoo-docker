fe6586f924d0989c1c7ac4ab9a505576cc94e41d
Jordi Riera <kender.jr@gmail.com>
2018-01-03 20:24:33 -0500

    fix ci

--- docker_files/run_test.sh
+++ docker_files/run_test.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+until pg_isready; do
+    echo "$(date) - waiting for postgres..."
+    sleep 1
+done
 odoo -c /etc/odoo/odoo.conf --db_host=db -r odoo -w odoo -d odoo -i main --stop-after-init --test-enable
