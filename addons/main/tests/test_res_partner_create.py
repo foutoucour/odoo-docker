f8fa88e55c496b1eacbadcb1b3a811491e3e4c2f
Jordi Riera <547282+foutoucour@users.noreply.github.com>
2018-01-12 09:56:04 -0500

    Add codacy coverage (#17)
    * Add codacy coverage
    
    * Add coverage badge

--- addons/main/tests/test_res_partner_create.py
+++ addons/main/tests/test_res_partner_create.py
@@ -1,22 +1,31 @@
 import logging
 
 from odoo.tests import common
+
 logger = logging.getLogger(__name__)
 
 
+def branches(i, x=False):
+    """Simple function with 2 branches"""
+    if i == 1:
+        res = True
+    if i == 2:
+        res = False
+
+    if x:
+        res = True
+
+    return res
+
+
 class TestResPartner(common.TransactionCase):
-    """Use case of creation of a partner."""
-    def setUp(self):
-        super(TestResPartner, self).setUp()
-        self.partner_pool = self.env['res.partner']
-        self.country_pool = self.env['res.country']
-        self.canada = self.country_pool.browse(39)
 
     def test_create(self):
+        """Test to check we talk with the db and other odoo.addons."""
         name = 'tpartner'
-        partner = self.partner_pool.create({
-            'name': name,
-            'state_id': 1,
-            'country_id': self.canada.id,
-        })
+        partner = self.env['res.partner'].create({'name': name})
         self.assertEqual(name, partner.name)
+
+    def test_branches(self):
+        """Test to see how branches are reported in cov reports"""
+        self.assertTrue(branches(1))
