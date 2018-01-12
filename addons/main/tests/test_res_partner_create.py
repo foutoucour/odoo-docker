cad85bd854448fcae9bc9d41c197f249828dfc83
Jordi Riera <jordi.riera@autodesk.com>
2018-01-12 09:41:17 -0500

    branches

--- addons/main/tests/test_res_partner_create.py
+++ addons/main/tests/test_res_partner_create.py
@@ -5,12 +5,16 @@ from odoo.tests import common
 logger = logging.getLogger(__name__)
 
 
-def branches(i):
+def branches(i, x=False):
     """Simple function with 2 branches"""
     if i == 1:
         res = True
     if i == 2:
         res = False
+
+    if x:
+        res = True
+
     return res
 
 
