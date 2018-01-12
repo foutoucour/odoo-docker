f33b89e8f58924377edddf9105c7548aac3275dd
Jordi Riera <jordi.riera@autodesk.com>
2018-01-12 08:45:07 -0500

    Add codacy coverage

--- docker_files/run_test.sh
+++ docker_files/run_test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd ${LOG_ODOO}
-pytest /mnt/extra-addons/ --junit-xml=/var/log/odoo/junit.xml --cov-branch --cov --cov-config /.coveragerc
+pytest /mnt/extra-addons/ --junit-xml=/var/log/odoo/junit.xml --cov-branch --cov --cov-config /.coveragerc --cov-report xml
